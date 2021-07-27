e00def550a52a69f449d322ae4a6777072ec0fd4
Mahdi <mahdighadami2@gmail.com>
2021-07-28 02:40:33 +0430

    add "counting_sort.py" code for making seperated type of executable SORTING program.

---
+++ counting_sort.py
@@ -0,0 +1,23 @@
+#Counting Sort
+def counting_sort(A):
+    m = max(A)
+    count = [0] * (m+1)
+
+    for x in A:
+        count[x] += 1
+
+    A=[]
+
+    for i in range(m+1):
+        A += [i] * count[i]
+
+    return A
+
+input_str = input('Enter "non negetive"!!! numbers (Use Comma to separate the numbers)\nExample: 9,5,6,44,50\nNumbers: ')
+input_str = input_str.replace(' ', '')
+A = list(map(int, input_str.split(',')))
+
+try:
+    print(counting_sort(A))
+except:
+    print("input is not valid")
