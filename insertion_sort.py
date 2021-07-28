a7f46c623251b91a6acabd7dde5341caa134a728
Mahdi <mahdighadami2@gmail.com>
2021-07-29 02:19:55 +0430

    add "insertion_sort.py" code for making seperated type of executable SORTING program.

---
+++ insertion_sort.py
@@ -0,0 +1,19 @@
+#Insertion Sort
+def insertion_sort(B):
+    for j in range(1, len(B)):
+        item = B[j]
+        i = j
+        while i>0 and item<B[i-1]:
+            B[i] = B[i-1]
+            i -= 1
+        B[i] = item
+    return(B)
+
+input_str = input('Enter numbers (Use Comma to separate the numbers)\nExample: 9,-5,6,44,50\nNumbers: ')
+input_str = input_str.replace(' ', '')
+B = list(map(int, input_str.split(',')))
+
+try:
+    print(insertion_sort(B))
+except:
+    print("input is not valid")
